5e02c878ea4d22d75f528d288045e2d140ebb477
zhanjh <zhanjh@126.com>
2018-04-04 15:24:34 +0800

    Fix jest & eslint

--- __tests__/ZselectTest.js
+++ __tests__/ZselectTest.js
@@ -1,17 +1,33 @@
 import {Zselect} from '../index.js';
 
 test('Zselect', () => {
-    const select = new Zselect();
+    const select = new Zselect({
+        name: 'user',
+        pattern: {
+            content: '#{name}'
+        }
+    });
 
     select.appendTo(document.body);
 
-    select.onInput(query => {
-    });
-
-    select.setItems([
+    select.showDropList([
         {name: 'item1'},
         {name: 'item2'}
     ]);
 
-    console.log(document.body.innerHTML);
+    expect(document.body.innerHTML).toBe(
+        '<div class="zselect">'
+         + '<div class="selected-wrap"> '
+         + '<div class="selected-list"></div> '
+         + '<input type="text" class="zinput"> '
+         + '</div> '
+         + '<div class="drop-wrap" style="position: absolute;">'
+         + '<ul>'
+         + '<li> '
+         + '<a href="javascript:;" class="drop-item" key="0"> item1 </a> '
+         + '</li> '
+         + '<li> '
+         + '<a href="javascript:;" class="drop-item" key="1"> item2 </a> '
+         + '</li></ul></div></div>'
+    );
 });
